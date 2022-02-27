b03113d9a2e0d64baefd8d25c310d88cc1f173d0
swasthi <swasthishetty01@gmail.com>
2022-02-27 18:46:03 +0530

    Updated add router

--- convert_csv.py
+++ convert_csv.py
@@ -8,12 +8,28 @@ def con(ret_x,ret_y,ret_w,ret_h,router_point,room_pass_mat):
     lst = list(itertools.product(x_coords, y_coords))
     output=[]
     for ele in lst:
-        shortestdist=(round(math.sqrt(((ele[0]-router_point[0])**2)+((ele[1]-router_point[1])**2)));
-        ele = ele + shortestdist,
-        room_pass_mat[0],room_pass_mat[1],room_pass_mat[2],room_pass_mat[3],room_pass_mat[4],room_pass_mat[5],) ##[480,300500,0,0,0,0,0,0]((shortestdist*0.42/12)+room_pass_mat[0]
+        ele = ele + (round(math.sqrt(((ele[0]-router_point[0])**2)+((ele[1]-router_point[1])**2))),
+        room_pass_mat[0],room_pass_mat[1],room_pass_mat[2],room_pass_mat[3],room_pass_mat[4],room_pass_mat[5],) ##[480,300500,0,0,0,0,0,0]
         output.append(ele)
     print(output)
     return(output)
 #con(480,300,120,120,(0,0),"WC")
 #   CONCRETE,        HBLOCK,           BRICK,          WOOD,              GLASS,           RCCROOF
 #room_pass_mat[0],room_pass_mat[1],room_pass_mat[2],room_pass_mat[3],room_pass_mat[4],room_pass_mat[5],
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
+#[480,300500,0,0,0,0,0,0]
