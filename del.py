05d01adaf37c829a6ea5e7a7cc485135ca09c062
swasthi <swasthishetty01@gmail.com>
2022-05-21 19:17:47 +0530

    added ai

--- del.py
+++
@@ -1,9 +0,0 @@
-import itertools
-import numpy as np
-inp = [(480, 300), (600, 420)]
-x_coords = [x for x in range(inp[0][0], inp[1][0] + 1,10)]
-y_coords = [y for y in range(inp[0][1], inp[1][1] + 1,15)]
-output = np.array(list(itertools.product(x_coords, y_coords)))
-print(x_coords)
-print(y_coords)
-np.savetxt("data.csv", output, delimiter = ",")
